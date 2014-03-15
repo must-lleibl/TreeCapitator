be180f60335bed7b0a2e1b0208b602e00177ae21
must-lleibl <mustafashahdzahan@gmail.com>
2014-03-16 00:55:32 +0400

    i dont know api 13 (((

---
+++ TreeCapitator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+__PocketMine Plugin__
+name=TreeCapitator
+description=Break trees faster breaking the lowest trunk
+version=getting started p1
+author=must and (PEMapModder)-if he wishes to help me (just say yes)
+class=TreeCap
+apiversion=12,13
+*/
+
+/*
+Here is the progress record:
+-p1//getting started\\
+*/
+
+
+class TreeCap implements Plugin {
